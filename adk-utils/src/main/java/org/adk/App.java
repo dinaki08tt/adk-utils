ae1f0c8fe4c332eec3c05224d837cd357293694f
adk <adk@adktta.com>
2018-07-08 20:39:27 +0530

    Initial Checkin

---
+++ adk-utils/src/main/java/org/adk/App.java
@@ -0,0 +1,21 @@
+package org.adk;
+
+import java.util.Base64;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+//        String plainClientCredentials="myusername:mypassword";
+//        String base64ClientCredentials = new String(Base64.encodeBase64(plainClientCredentials.getBytes()));
+//
+//        HttpHeaders headers = getHeaders();
+//        headers.add("Authorization", "Basic " + base64ClientCredentials);
+    }
+}
