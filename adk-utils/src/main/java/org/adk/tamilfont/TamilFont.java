ff316cf93e49e73dd6deb0992801ef4b73bf5b10
adk <adk@adk-desktop>
2018-10-02 21:04:00 +0530

    தமிழன்டா fonts

---
+++ adk-utils/src/main/java/org/adk/tamilfont/TamilFont.java
@@ -0,0 +1,16 @@
+package org.adk.tamilfont;
+
+public class TamilFont {
+
+	public static void main(String[] args) {
+		for(int i=0;i<1000;i++) {
+			System.out.println("தமிழ்");
+			System.out.println("தமிழர் அறம்");
+			System.out.println("ஞானிக்கு இல்லை இன்பமும் துன்பமும்");
+			System.out.println("தமிழன்டா");			
+		}
+
+		
+	}
+
+}
