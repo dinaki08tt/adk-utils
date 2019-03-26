ca515de5ae3bb57b78a94fc96abcf69066f8c217
ADK <adk@adk.com>
2019-03-26 13:19:38 +0530

    zip file

--- bootstater/src/main/java/com/person/bootstater/services/compress/CompressMain.java
+++ bootstater/src/main/java/com/person/bootstater/services/compress/CompressMain.java
@@ -1,5 +1,6 @@
 package com.person.bootstater.services.compress;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,9 +8,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
@@ -101,4 +109,55 @@ public class CompressMain {
 
 	    return outputFile;
 	}
+	
+	
+	 
+    public static void unZipIt() {
+    	  //Open the file
+        try
+        {
+        	ZipFile file = new ZipFile("files.zip");
+            FileSystem fileSystem = FileSystems.getDefault();
+            //Get file entries
+            Enumeration<? extends ZipEntry> entries = file.entries();
+//            file.
+             
+            //We will unzip files in this folder
+            String uncompressedDirectory = "uncompressed/";
+            Files.createDirectory(fileSystem.getPath(uncompressedDirectory));
+             
+            //Iterate over entries
+            while (entries.hasMoreElements())
+            {
+                ZipEntry entry = entries.nextElement();
+                //If directory then create a new directory in uncompressed folder
+                if (entry.isDirectory())
+                {
+                    System.out.println("Creating Directory:" + uncompressedDirectory + entry.getName());
+                    Files.createDirectories(fileSystem.getPath(uncompressedDirectory + entry.getName()));
+                }
+                //Else create the file
+                else
+                {
+                    InputStream is = file.getInputStream(entry);
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    String uncompressedFileName = uncompressedDirectory + entry.getName();
+                    Path uncompressedFilePath = fileSystem.getPath(uncompressedFileName);
+                    Files.createFile(uncompressedFilePath);
+                    FileOutputStream fileOutput = new FileOutputStream(uncompressedFileName);
+                    while (bis.available() > 0)
+                    {
+                        fileOutput.write(bis.read());
+                    }
+                    fileOutput.close();
+                    System.out.println("Written :" + entry.getName());
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+	
 }
---
+++ bootstater/src/main/java/com/person/bootstater/services/compress/CreatePasswordProtectedZipExample.java
@@ -0,0 +1,119 @@
+package com.person.bootstater.services.compress;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.FileHeader;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+ 
+public class CreatePasswordProtectedZipExample
+{
+	
+    private static final String PASS = "threeidiots27";
+	private static final String ZIP_FILE = "/tech.zip";
+    private static final String UNZIP_PATH = "/bak";
+
+
+	public static void main(String[] args){
+//    	zipIt();
+    	unZipEncrypted();
+    }
+    
+    public static void zipIt() {
+    	
+    	 try {
+             //This is name and path of zip file to be created
+             net.lingala.zip4j.core.ZipFile zipFile = new net.lingala.zip4j.core.ZipFile(ZIP_FILE);
+              
+             //Add files to be archived into zip file
+             ArrayList<File> filesToAdd = new ArrayList<File>();
+             filesToAdd.add(new File("D:\\zipFolder\\bk.html")); //zip file content
+              
+             ZipParameters parameters = zipParameters();
+              
+             //Now add files to the zip file
+             zipFile.addFiles(filesToAdd, parameters);
+         }
+         catch (ZipException e)
+         {
+             e.printStackTrace();
+         }
+    }
+
+	private static ZipParameters zipParameters() {
+		//Initiate Zip Parameters which define various properties
+		 ZipParameters parameters = new ZipParameters();
+		 parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE); // set compression method to deflate compression
+		  
+		 //DEFLATE_LEVEL_FASTEST     - Lowest compression level but higher speed of compression
+		 //DEFLATE_LEVEL_FAST        - Low compression level but higher speed of compression
+		 //DEFLATE_LEVEL_NORMAL  - Optimal balance between compression level/speed
+		 //DEFLATE_LEVEL_MAXIMUM     - High compression level with a compromise of speed
+		 //DEFLATE_LEVEL_ULTRA       - Highest compression level but low speed
+		 parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+		  
+		 //Set the encryption flag to true
+		 parameters.setEncryptFiles(true);
+		  
+		 //Set the encryption method to AES Zip Encryption
+		 parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+		  
+		 //AES_STRENGTH_128 - For both encryption and decryption
+		 //AES_STRENGTH_192 - For decryption only
+		 //AES_STRENGTH_256 - For both encryption and decryption
+		 //Key strength 192 cannot be used for encryption. But if a zip file already has a
+		 //file encrypted with key strength of 192, then Zip4j can decrypt this file
+		 parameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+		  
+		 //Set password
+		 parameters.setPassword(PASS);
+		return parameters;
+	}
+    
+    public static void unZipEncrypted() {
+    	
+    	try {
+			net.lingala.zip4j.core.ZipFile zipFile = new net.lingala.zip4j.core.ZipFile(ZIP_FILE);
+			
+			if(zipFile.isEncrypted()) {
+				zipFile.setPassword(PASS);
+			}
+//			UnzipParameters unzipParameters = new UnzipParameters();
+			
+			
+		//	unzipParameters.
+			
+		//	zipFile.extractAll(UNZIP_PATH, null);
+			
+			@SuppressWarnings("unchecked")
+            List<FileHeader> fileHeaders = zipFile.getFileHeaders();
+
+            for(FileHeader fileHeader : fileHeaders) {
+                if (fileHeader.isDirectory()) {
+                    zipFile.extractFile(fileHeader, UNZIP_PATH);
+                }else {
+                	zipFile.extractAll(UNZIP_PATH);
+                }
+            }
+    	
+    	} catch (ZipException e) {
+			e.printStackTrace();
+		}
+    	    	
+    }
+    
+}
