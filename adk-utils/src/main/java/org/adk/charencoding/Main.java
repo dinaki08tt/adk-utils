0dd5fb8ee0b9cec4fce9edfe6e3549f67e9c1517
alastorcrouch <alastorcrouch@ac.org>
2020-01-23 07:43:23 +0530

    unicode

---
+++ adk-utils/src/main/java/org/adk/charencoding/Main.java
@@ -0,0 +1,10 @@
+package org.adk.charencoding;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		String unicode = "\u00A6"; //'BROKEN BAR' 
+		System.out.println(unicode);
+	}
+}
